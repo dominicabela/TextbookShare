4cf898aa47d4d24a06b1246498f6d32fb22d9c29
Dominic Abela <d.j.m.abela@gmail.com>
2018-03-14 15:53:12 -0400

    Initial commit

---
+++ store/session/actions.js
@@ -0,0 +1,71 @@
+export const restoreSession = () => {
+  return (dispatch) => {
+    dispatch(sessionRestoring())
+
+    let unsubscribe = firebaseService.auth()
+      .onAuthStateChanged(user => {
+        if (user) {
+          dispatch(sessionSuccess(user))
+          unsubscribe()
+        } else {
+          dispatch(sessionLogout())
+          unsubscribe()
+        }
+      })
+  }
+}
+
+export const loginUser = (email, password) => {
+  return (dispatch) => {
+    dispatch(sessionLoading())
+
+    firebaseService.auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        dispatch(sessionError(error.message))
+      })
+
+    let unsubscribe = firebaseService.auth()
+      .onAuthStateChanged(user => {
+        if (user) {
+          dispatch(sessionSuccess(user))
+          unsubscribe()
+        }
+      })
+  }
+}
+
+export const signupUser = (email, password) => {
+  return (dispatch) => {
+    dispatch(sessionLoading())
+
+    firebaseService.auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => {
+        dispatch(sessionError(error.message));
+      })
+
+    let unsubscribe = firebaseService.auth()
+      .onAuthStateChanged(user => {
+        if (user) {
+          dispatch(sessionSuccess(user))
+          unsubscribe()
+        }
+      })
+  }
+}
+
+export const logoutUser = () => {
+  return (dispatch) => {
+    dispatch(sessionLoading())
+
+    firebaseService.auth()
+      .signOut()
+      .then(() => {
+        dispatch(sessionLogout())
+      })
+      .catch(error => {
+        dispatch(sessionError(error.message))
+      })
+  }
+}
