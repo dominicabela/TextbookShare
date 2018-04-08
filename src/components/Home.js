96c018ee0f5a253d95957e94f06a16b51bd81557
Dominic Abela <d.j.m.abela@gmail.com>
2018-04-08 03:53:45 -0400

    Add responsive menu bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,8 @@
-import React, { Component } from 'react';
-import { db } from '../firebase';
+import React from 'react';
 
-class HomePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      books: null,
-    };
-  }
-
-  componentDidMount() {
-    db.onceGetBooks().then(snapshot =>
-      this.setState(() => ({ books: snapshot.val() }))
-    );
-  }
-
-  render() {
-
-    const { books } = this.state;
-
-    return (
-      <div>
-        <h1 className='home'>Home Page</h1>
-        { !!books && <UserList books={books} /> }
-      </div>
-    );
-  }
-}
-
-const UserList = ({ books }) =>
+const HomePage = () =>
   <div>
-    <h2>Available Books:</h2>
-    <table>
-      <tr>
-        <th>ISBN</th>
-        <th>Price</th>
-      </tr>
-      {Object.keys(books).map(key =>
-        <tr>
-          <td>{key}</td>
-          <td>{books[key].price}</td>
-        </tr>
-      )}
-    </table>
+    <h1 className='home'>Home Page</h1>
   </div>
 
 export default HomePage;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,6 +5,12 @@ import AuthUserContext from './AuthUserContext';
 import SignOutButton from './SignOut';
 import * as routes from '../constants/routes';
 
+import ResponsiveMenu from 'react-responsive-navbar';
+import FaAlignJustify from 'react-icons/lib/fa/';
+
+var menuIcon = require('react-icons/lib/fa/bars');
+var closeIcon = require('react-icons/lib/fa/close');
+
 const Navigation = () =>
   <AuthUserContext.Consumer>
     {authUser => authUser
@@ -14,18 +20,38 @@ const Navigation = () =>
   </AuthUserContext.Consumer>
 
 const NavigationAuth = () =>
-  <ul className='nav'>
-    <li className='logo'><Link to={routes.HOME}><img src={require('../res/images/logo.png')} alt='Home'/></Link></li>
-    <li className='nav-tab'><Link to={routes.ACCOUNT}>Account</Link></li>
-    <li className='nav-tab'><Link to={routes.SEARCH}>Search</Link></li>
-    <li className='nav-tab'><Link to={routes.SELL}>Sell</Link></li>
-    <li className='signOutButton'><SignOutButton /></li>
-  </ul>
+  <div className='nav'>
+    <div className='logo'><Link to={routes.HOME}><img src={require('../res/images/logo.png')} alt='Home'/></Link></div>
+    <ResponsiveMenu
+          menuOpenButton={
+            <div>
+              {React.createElement(menuIcon, null)}
+            </div>
+          }
+          menuCloseButton={
+            <div>
+              {React.createElement(closeIcon, null)}
+            </div>
+          }
+          changeMenuOn="500px"
+          largeMenuClassName="nav-tab"
+          smallMenuClassName="nav-collapsed"
+          menu={
+            <ul>
+              <li className='nav-tab'><Link to={routes.ACCOUNT}>Account</Link></li>
+              <li className='nav-tab'><Link to={routes.SEARCH}>Search</Link></li>
+              <li className='nav-tab'><Link to={routes.SELL}>Sell</Link></li>
+              <li className='signOutButton'><SignOutButton /></li>
+            </ul>
+          }
+        />
+  </div>
 
 const NavigationNonAuth = () =>
   <ul className='nav'>
     <li className='logo'><Link to={routes.HOME}><img src={require('../res/images/logo.png')} alt='Home'/></Link></li>
     <li className='navButton'><Link to={routes.SIGN_IN}>Sign In</Link></li>
   </ul>
+
 
 export default Navigation;
